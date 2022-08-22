fd8799de77c33cf987c45fddba9481de61313803
Chad Horne <chad.horne@wavicledata.com>
2022-08-22 14:55:10 -0500

    extract code to function module

--- src/functions.py
+++ src/functions.py
@@ -1,21 +1,13 @@
-from email.policy import strict
-import os
 import json
 import pandas as pd
 
-# current_directory = os.path.dirname(__file__)
-# file_path = os.path.join(current_directory, '..\\Explain Plan generated.JSON')
-
-# with open(file_path, 'r') as f:
-#     data = json.load(f, strict=False)
-
-# print(data)
-
+# Parses the Explain Plan json file and returns two dataframes,
+# one with the global stats and one with the operations
 def readExplainPlanFile(file_path):
     with open(file_path, 'r') as f:
         data = json.load(f, strict=False)
 
         global_stats_df = pd.json_normalize(data['GlobalStats'])
-        operations_df = pd.json_normalize(*data['Operationts'])
+        operations_df = pd.json_normalize(*data['Operations'])
 
     return global_stats_df, operations_df
