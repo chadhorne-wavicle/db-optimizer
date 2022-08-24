ac7dec2a31886ff419da8e9e06f27f94c0fbcdd1
Chad Horne <chad.horne@wavicledata.com>
2022-08-24 16:27:11 -0500

    document script goals in comments

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# The functions defined in this script aim to provide the following functionality:
+
+# 1.	Find out all the SELECT queries running in snowflake(Query History.sql)
+# 2.	Find out and group, similar queries from it. (We also created a python script if you want to have a look -- Python-SimilarQueries_v2.ipynb)
+# 3.	Generate the json readable format from explain plan (Explain Plan_Query.sql) 
+# 4.	Find out tables, filters conditions, stats, etc from the explain plan so generated (Explain Plan generated.json)
+# 5.	Suggest Wide table creation for the similar queries 
+
 import sys
 from src import functions as fx
 
